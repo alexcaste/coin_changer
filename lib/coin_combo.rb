4a606f828b0e1fbe1eb5a1e17e16502b3e1923b5
Alex Caste & David Abel <student@epicodus.com>
2015-04-23 11:02:25 -0700

    2nd spec written.  commented out 1st spec

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -1,15 +1,40 @@
 class Fixnum
 
     define_method(:changer) do
+    quarters = 0
+    dimes = 0
+    nickels = 0
+    pennies = 0
+    counter = self
 
-    change = self
 
-        if change > 0
-            change = change./(25)
-        else return "your poor"
+        until counter < 25
+            counter = counter.-(25)
+            quarters = quarters.+(1)
         end
 
-    change
+        until counter < 10
+            counter = counter.-(10)
+            dimes = dimes.+(1)
+        end
+
+        until counter < 5
+            counter = counter.-(5)
+            nickles = nickels.+(1)
+        end
+
+        until counter < 1
+            counter = counter.-(1)
+            pennies = pennies.+(1)
+        end
+
+        # if counter > 0
+        #     counter = counter./(25)
+        # else return "your poor"
+        # end
+
+
+
     end
 
 end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -5,8 +5,13 @@ require('coin_combo')
 describe('Fixnum#changer')do
 
     #it determines the numbers of quarters within a number
-    it("takes a given number and divides by 25 to determine the number of quarters") do
-        expect(75.changer()).to(eq(3))
+    # it("takes a given number and divides by 25 to determine the number of quarters") do
+    #     expect(75.changer()).to(eq(3))
+    # end
+
+    #it takes a number and returns the number of quarters, dimes, nickels & pennies to make up the number
+    it("takes a number and returns the quarters, dimes, nickels, pennies in number") do
+        expect(103.changer()).to(eq("3 quarters, 2 dimes, 1 nickles, 3 pennies"))
     end
 
 end
