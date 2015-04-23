0501a82ae165e1299cb444c9cbb76bc0c779d50e
Alex Caste & David Abel <student@epicodus.com>
2015-04-23 10:09:25 -0700

    first spec fail

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -1,3 +1,12 @@
 require('rspec')
 require('pry')
 require('coin_combo')
+
+describe('Fixnum#changer')do
+
+    #it determines the numbers of quarters within a number
+    it("takes a given number and divides by 25 to determine the number of quarters") do
+        expect(78).changer().to(eq(3))
+    end
+
+end
