388230d56d0962a0ff2b98c6677ff870611b2e35
Alex Caste & David Abel <student@epicodus.com>
2015-04-23 09:47:08 -0700

    initial files added

---
+++ app.rb
@@ -0,0 +1,12 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/coin_combo')
+also_reload('lib/**/*.rb')
+
+get('/') do
+    erb(:index)
+end
+
+get('/change')
+    erb(:change)
+end
